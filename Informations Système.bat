304547d0aeb7f45124b209db3e1fb331571c376b
Andrew33340 <76187819+Andrew33340@users.noreply.github.com>
2021-01-30 23:53:38 +0100

    Add files via upload

---
+++ Informations Système.bat
@@ -0,0 +1,8 @@
+echo off
+cd C:\Windows\System32
+cls
+echo ===========================================
+echo  Informations Système Logiciel et Phisique
+echo ===========================================
+systeminfo
+pause
